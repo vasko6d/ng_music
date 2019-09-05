8379f7aa61c098436174e7dfb80305ac4554b947
David Vasko <vasko6d@gmail.com>
2019-09-05 00:19:58 -0700

    testing and thorough fixing and documentation of getting POJO to load the sample data after upgrades

--- README.md
+++ README.md
@@ -33,14 +33,15 @@ bundle install
 bundle update
 ```
 
-- Create the user for the application to use and grant necessary permissions
+- Create the user for the application to use and grant necessary permissions.
 
 ```
 CREATE USER 'ng_user'@'localhost' IDENTIFIED BY 'ng';
-GRANT ALL PRIVILEGES ON ng_music . * TO 'ng_user'@'localhost';
+GRANT ALL PRIVILEGES ON ng_music.* TO 'ng_user'@'localhost' IDENTIFIED BY 'ng';
+GRANT FILE ON *.* to 'ng_user'@'localhost' IDENTIFIED BY 'ng'; --needed to acces the server side files (not database sepcific so must be *.*)
 ```
 
-- Create a test user to use in the web app (cannot add user from web app at this time). Alternatively, if you load the sample data use oen of teh sample users.
+- Create a test user to use in the web app (cannot add user from web app at this time). Alternatively, if you load the sample data use oen of the sample users.
 
 ```
 INSERT INTO ng_users(username,password) VALUES ('ng_test','ngt');
@@ -54,10 +55,39 @@ rake db:create
 rake db:schema:load
 ```
 
-- Add test data in if desired using the provided POJO.
+## Add test data in if desired using the provided POJO.
+
+- **IMPORTANT**: If your mysql has the "--secure-file-priv" option you many need to move the files or disable the option: see https://superuser.com/questions/1088512/how-to-disable-secure-file-priv-mysql-ubuntu.
 
 ```
-<Will do later>
+sudo echo "secure_file_priv=\"\"" | sudo tee -a /etc/mysql/mysql.conf.d/mysqld.cnf
+service mysql stop
+service mysql start
+```
+
+I also needed to let mysql have read acces to my svn folder, see: https://stackoverflow.com/questions/4215231/load-data-infile-error-code-13/19511472.
+
+```
+[/etc/apparmor.d/usr.sbin.mysqld]
+...
+#Allow local SVN Read Acces
+  /home/david/svn/** r,
+...
+}
+```
+
+and reload AppArmour
+
+```
+sudo /etc/init.d/apparmor reload
+```
+
+Actually load the test data by compiling the java file and running it (from the ng-music folder). Dont forget since we are using a mysql jdbc we need the mysql-connecter.jar in the classpath.
+
+```
+javac ./db/util/ng_music_db_loader.java
+java -cp "<path-to-mysql-connector-java-8.0.17.jar>:./db/util" ng_music_db_loader <full-path-to: ng-music>/db/util/ ng_user ng
+
 ```
 
 ## Running Development Server
--- db/util/ng_music_db_loader.java
+++ db/util/ng_music_db_loader.java
@@ -1,87 +1,97 @@
+
 //Import required packages
 import java.sql.*;
 
 public class ng_music_db_loader {
-   // JDBC driver name and database URL
-   static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";  
-   static final String DB_URL = "jdbc:mysql://localhost/ng_music?useSSL=false";
-   
-   //  Database credentials
-   static final String USER = "***";
-   static final String PASS = "***";
-   
-   public static void main(String[] args) {
-   Connection conn = null;
-   Statement stmt = null;
-   try{
-      //Register JDBC driver
-      Class.forName("com.mysql.jdbc.Driver");
+    // JDBC driver name and database URL
+    static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
+    static final String DB_URL = "jdbc:mysql://localhost/ng_music?useSSL=false&serverTimezone=UTC";
 
-      //Open a connection
-      System.out.print("Connecting to database...");
-      conn = DriverManager.getConnection(DB_URL,USER,PASS);
-      System.out.println("done.");
+    public static void main(String[] args) {
 
-      //declare statement, sql and file_loc variables to be reused in sql executions
-      stmt = conn.createStatement();
-      String sql;
-      String file_loc;
+        // Simple parsing of args
+        // Where the sample input files are: [ng_users.txt, ng_albums.txt,
+        // ng_singers.txt]
+        String sampleFileFolder = args[0];
+        // Database Credentials
+        String user = args[1];
+        String pass = args[2];
 
-      //ADD ng_users through local file ng_users.txt
-      System.out.print("Creating statement to add ng_users...");
-      file_loc = "'C:/Users/David/Desktop/NextGate/TakeHomeAssignment/resource/ng_users.txt'";
-      sql = "LOAD DATA LOCAL INFILE " + file_loc + " INTO TABLE ng_users"
-          + " FIELDS TERMINATED BY '|'"
-          + " LINES TERMINATED BY '\r\n'"
-          + " IGNORE 1 LINES"
-          + " (@var1,@var2)"
-          + " SET username = RTRIM(@var1), password = RTRIM(@var2);";
-      stmt.execute(sql);
-      System.out.println("done.");
+        Connection conn = null;
+        Statement stmt = null;
+        try {
+            // Register JDBC driver
+            Class.forName(JDBC_DRIVER);
 
-      //ADD ng_singers throuhg local file ng_singers.txt
-      System.out.print("Creating statement to add ng_singers...");
-      file_loc = "'C:/Users/David/Desktop/NextGate/TakeHomeAssignment/resource/ng_singers.txt'";
-      sql = "LOAD DATA LOCAL INFILE " + file_loc + " INTO TABLE ng_singers" 
-          + " FIELDS TERMINATED BY '|'"
-          + " LINES TERMINATED BY '\r\n'"
-          + " IGNORE 1 LINES"
-          + " (@var1,@var2,sex)"
-          + " SET name = RTRIM(REPLACE(@var1,char(9),'')), dob = STR_TO_DATE( RTRIM( REPLACE( @var2,char(9),'')),'%Y%m%d');";
-      stmt.execute(sql);
-      System.out.println("done.");      
+            // Open a connection
+            System.out.print("Connecting to database...");
+            conn = DriverManager.getConnection(DB_URL, user, pass);
+            System.out.println("done.");
 
-      //ADD ng_albums throuhg local file ng_singers.txt
-      System.out.print("Creating statement to add ng_albums...");
-      file_loc = "'C:/Users/David/Desktop/NextGate/TakeHomeAssignment/resource/ng_albums.txt'";
-      sql = "LOAD DATA LOCAL INFILE " + file_loc + " INTO TABLE ng_albums"
-          + " FIELDS TERMINATED BY '|'"
-          + " LINES TERMINATED BY '\r\n'"
-          + " IGNORE 1 LINES" 
-          + " (@col1,@col2,@col3,@col4)"
-          + " SET ng_singers_id = (SELECT ng_singers_id FROM ng_singers WHERE name = RTRIM(REPLACE(@col1,char(9),''))),"
-          + "     album_name    = RTRIM(REPLACE(@col2,char(9),'')),"
-          + "     release_year = RTRIM(REPLACE(@col3,char(9),'')),"
-          + "     record_company = RTRIM(@col4);";
-      stmt.execute(sql);
-      System.out.println("done.");  
+            // declare statement, sql and file_loc variables to be reused in sql executions
+            stmt = conn.createStatement();
+            String sql;
+            String file_loc;
 
-      //Clean-up environment
-      stmt.close();
-      conn.close();
-      }
+            // ADD ng_users through local file ng_users.txt
+            System.out.print("Creating statement to add ng_users...");
+            file_loc = sampleFileFolder + "ng_users.txt";
+            sql = "LOAD DATA INFILE '" + file_loc + "' INTO TABLE ng_users" + " FIELDS TERMINATED BY '|'"
+                    + " LINES TERMINATED BY '\r\n'" + " IGNORE 1 LINES" + " (@var1,@var2)"
+                    + " SET username = RTRIM(@var1), password = RTRIM(@var2);";
+            stmt.execute(sql);
+            System.out.println("done.");
 
-   //Handle errors for JDBC
-   catch(SQLException se) {se.printStackTrace(); }
+            // ADD ng_singers throuhg local file ng_singers.txt
+            System.out.print("Creating statement to add ng_singers...");
+            file_loc = sampleFileFolder + "ng_singers.txt";
+            sql = "LOAD DATA INFILE '" + file_loc + "' INTO TABLE ng_singers" + " FIELDS TERMINATED BY '|'"
+                    + " LINES TERMINATED BY '\r\n'" + " IGNORE 1 LINES" + " (@var1,@var2,sex)"
+                    + " SET name = RTRIM(REPLACE(@var1,char(9),'')), dob = STR_TO_DATE( RTRIM( REPLACE( @var2,char(9),'')),'%Y%m%d');";
+            stmt.execute(sql);
+            System.out.println("done.");
 
-   //Handle errors for Class.forName
-   catch(Exception e) {e.printStackTrace(); }
+            // ADD ng_albums throuhg local file ng_singers.txt
+            System.out.print("Creating statement to add ng_albums...");
+            file_loc = sampleFileFolder + "ng_albums.txt";
+            sql = "LOAD DATA INFILE '" + file_loc + "' INTO TABLE ng_albums" + " FIELDS TERMINATED BY '|'"
+                    + " LINES TERMINATED BY '\r\n'" + " IGNORE 1 LINES" + " (@col1,@col2,@col3,@col4)"
+                    + " SET ng_singers_id = (SELECT ng_singers_id FROM ng_singers WHERE name = RTRIM(REPLACE(@col1,char(9),''))),"
+                    + "     album_name    = RTRIM(REPLACE(@col2,char(9),'')),"
+                    + "     release_year = RTRIM(REPLACE(@col3,char(9),''))," + "     record_company = RTRIM(@col4);";
+            stmt.execute(sql);
+            System.out.println("done.");
 
-   //finally block used to close resources
-   finally{
-      try {if(stmt!=null) stmt.close();} catch(SQLException se2){/*ignored*/ }
-      try {if(conn!=null) conn.close();} catch(SQLException se){ se.printStackTrace();}}//end finally
-      
-   System.out.println("ng_music_db_loader Main Complete.");
-}//end main
-}//end ng_music_db_loader
+            // Clean-up environment
+            stmt.close();
+            conn.close();
+        }
+
+        // Handle errors for JDBC
+        catch (SQLException se) {
+            se.printStackTrace();
+        }
+
+        // Handle errors for Class.forName
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        // finally block used to close resources
+        finally {
+            try {
+                if (stmt != null)
+                    stmt.close();
+            } catch (SQLException se2) {
+                /* ignored */ }
+            try {
+                if (conn != null)
+                    conn.close();
+            } catch (SQLException se) {
+                se.printStackTrace();
+            }
+        } // end finally
+
+        System.out.println("ng_music_db_loader Main Complete.");
+    }// end main
+}// end ng_music_db_loader
